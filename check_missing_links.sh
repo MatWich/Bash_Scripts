badd731579cfa7d0c5322821723b269569fef29d
mateuszwicherkski <wicherskimateusz@gmail.com>
2022-09-11 18:52:49 +0200

    refactor

--- check_missing_links.sh
+++ check_missing_links.sh
@@ -1,29 +1,24 @@
 #!/bin/bash
 
-function check_arguments {
-    if [ "$#" - ne 1 ];  then 
-        echo "Provide path that you want to test"
-    fi
-}
-
 function check {
-    RC=0
-    if check_arguments; then
-        for entry in $1/*; do 
-        # echo "check $entry"
-        if [ -d ${entry} ]; then
-            check $entry
-        else 
-            if [ -L ${entry} ]; then
-                target=$(readlink $"entry")
-                if [ ! -e "$target" ]; then
-                    echo "BROKEN LINK FROM $entry -> $target"
+    if [[ "$#" -ne 1 ]]; then 
+        echo "Provide path that need to be check"
+        exit 1
+    else 
+        if check_arguments; then
+            for entry in $1/*; do 
+            # echo "check $entry"
+            if [ -d ${entry} ]; then
+                check $entry
+            else 
+                if [ -L ${entry} ]; then
+                    target=$(readlink $"entry")
+                    if [ ! -e "$target" ]; then
+                        echo "BROKEN LINK FROM $entry -> $target"
+                    fi
                 fi
             fi
-        fi
-        done
-    else 
-        exit 1
+            done
 }
 
 check $1
