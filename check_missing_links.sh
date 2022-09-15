aa906ce997304c0331de73cc137cdfde86980234
mateuszwicherkski <wicherskimateusz@gmail.com>
2022-09-15 07:52:36 +0200

    Cheking artgument fix

--- check_missing_links.sh
+++ check_missing_links.sh
@@ -1,24 +1,36 @@
 #!/bin/bash
 
-function check {
-    if [[ "$#" -ne 1 ]]; then 
+SUCCESS=1
+FAILED=0
+
+function arguments_check {
+    local RC=${SUCCESS}
+    if [[ "$1" -ne 1 ]]; then 
         echo "Provide path that need to be check"
+        echo "Example: ./check_broken_links.sh /opt"
+        RC=${FAILED}
+    fi
+    return ${RC}
+}
+
+function check {
+    if arguments_check "$#"; then
         exit 1
-    else 
-        if check_arguments; then
-            for entry in $1/*; do 
-            # echo "check $entry"
-            if [ -d ${entry} ]; then
-                check $entry
-            else 
-                if [ -L ${entry} ]; then
-                    target=$(readlink $"entry")
-                    if [ ! -e "$target" ]; then
-                        echo "BROKEN LINK FROM $entry -> $target"
-                    fi
+    fi
+
+    for entry in $1/*; do 
+        # echo "check $entry"
+        if [ -d ${entry} ]; then
+            check $entry
+        else 
+            if [ -L ${entry} ]; then
+                target=$(readlink $"entry")
+                if [ ! -e "$target" ]; then
+                    echo "BROKEN LINK FROM $entry -> $target"
                 fi
             fi
-            done
+        fi
+    done
 }
 
 check $1
